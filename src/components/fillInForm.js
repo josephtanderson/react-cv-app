ae605f56df20d0080f134853be6fe03361ce6c08
Joseph Anderson <josephtanderson91@gmail.com>
2022-04-26 17:20:29 -0700

    simplified fillin and reset func

--- src/components/fillInForm.js
+++ src/components/fillInForm.js
@@ -1,25 +1,18 @@
 let inputType = (item) => {
     if ( item !== "phone" && item !== "email") {return "text"};
-    if (item == "phone") {return "tel"};
+    if (item === "phone") {return "tel"};
     return item
 }
-
 
 export default function fillInForm(item, list, setMethod) {
     return <form onSubmit={(e) => { 
     e.preventDefault();
-    let items = Object.keys(list);
-    let temp = {};
-    items.map(x => {
-        temp[x] = list[x];
-    });
-temp[item] = e.target[0].value;
-    setMethod(temp)
-}}>
+    setMethod({ ...list, [item]: e.target[0].value })
+    }}>
     <input
     name={item}
     type={inputType(item)} 
-    placeholder={item.split('').map((x, i) => i == 0 ? x.toUpperCase() : x).join('')}
+    placeholder={item.split('').map((x, i) => i === 0 ? x.toUpperCase() : x).join('')}
     onChange={(e) => {
         e.preventDefault();
     }}
@@ -30,11 +23,5 @@ temp[item] = e.target[0].value;
 }
 
 export function resetForm(item, list, setMethod) {
-    let items = Object.keys(list);
-    let temp = {};
-    items.map(x => {
-        temp[x] = list[x];
-    });
-temp[item] = '';
-    setMethod(temp)
+    setMethod({ ...list, [item]: '' })
 }
